394095303ca30c58e2957545541020b8ee0d439f
Antoine Moreaux <moreaux.antoine@gmail.com>
2018-03-17 13:30:17 +0100

    add none to remove theme

--- popup.js
+++ popup.js
@@ -1,8 +1,12 @@
-function changeTheme(theme) {
+function changeTheme(theme, version) {
+  let script;
   console.log('>>>>>>>>>>>', theme);
-  const url = chrome.extension.getURL(`themes/2.x/theme-${theme}.css`);
-  console.log('>>>>>>>>>>>', url);
-  const script = `var link = document.createElement("link"); link.href = "${url}"; link.type = "text/css"; link.rel = "stylesheet"; document.getElementsByTagName("head")[0].appendChild(link);`;
+  if (theme === 'none') {
+    script = `var elm = document.getElementById('theme-swagger-by-extension'); if(elm)elm.parentNode.removeChild(elm);`;
+  } else {
+    const url = chrome.extension.getURL(`themes/${version}/theme-${theme}.css`)
+    script = `var link = document.createElement("link"); link.href = "${url}"; link.type = "text/css"; link.rel = "stylesheet"; link.id = "theme-swagger-by-extension"; document.getElementsByTagName("head")[0].appendChild(link);`;
+  }
   chrome.tabs.executeScript({
     code: script
   });
@@ -10,26 +14,38 @@ function changeTheme(theme) {
 
 function getSavedTheme(callback) {
   chrome.storage.sync.get('swagger-theme', (theme) => {
-    callback(chrome.runtime.lastError ? null : theme);
+    chrome.storage.sync.get('swagger-version', (version) => {
+      callback(chrome.runtime.lastError ? null : {
+        'swagger-theme': theme['swagger-theme'],
+        'swagger-version': version['swagger-version'] || '2.x'
+      });
+    });
   });
 }
 
-function saveTheme(theme) {
-  chrome.storage.sync.set({ 'swagger-theme': theme });
+function saveTheme(theme, swaggerVersion) {
+  chrome.storage.sync.set({ 'swagger-theme': theme, 'swagger-version': swaggerVersion });
 }
 
 document.addEventListener('DOMContentLoaded', () => {
   const dropdown = document.getElementById('dropdown');
+  const swaggerVersion = document.getElementById('swagger-version');
 
-  getSavedTheme((theme) => {
-    if (theme) {
-      changeTheme(theme['swagger-theme']);
-      dropdown.value = theme;
+  getSavedTheme((params) => {
+    if (params) {
+      changeTheme(params['swagger-theme'], params['swagger-version']);
+      dropdown.value = params['swagger-theme'];
+      swaggerVersion.value = params['swagger-version'];
     }
   });
 
   dropdown.addEventListener('change', () => {
-    changeTheme(dropdown.value);
-    saveTheme(dropdown.value);
+    changeTheme(dropdown.value, swaggerVersion.value);
+    saveTheme(dropdown.value, swaggerVersion.value);
+  });
+
+  swaggerVersion.addEventListener('change', () => {
+    changeTheme(dropdown.value, swaggerVersion.value);
+    saveTheme(dropdown.value, swaggerVersion.value);
   });
 });
